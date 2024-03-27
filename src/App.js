57b9a29cd63fe7c482ad039700e1b7975e851a5f
Sajjad Akhtar <sajjad.akhtar@funavry.com>
2024-03-27 14:56:12 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { init, useConnectWallet } from '@web3-onboard/react'
+import injectedModule from '@web3-onboard/injected-wallets'
+import { ethers } from 'ethers'
+
+// Sign up to get your free API key at https://explorer.blocknative.com/?signup=true
+// Required for Transaction Notifications and Transaction Preview
+const apiKey = '1730eff0-9d50-4382-a3fe-89f0d34a2070'
+
+const injected = injectedModule()
+
+const infuraKey = 'e200479dc89a4072a05c7aa1c0f83147'
+const rpcUrl = `https://mainnet.infura.io/v3/${infuraKey}`
+
+// initialize Onboard
+init({
+  apiKey,
+  wallets: [injected],
+  chains: [
+    {
+      id: '0x1',
+      token: 'ETH',
+      label: 'Ethereum Mainnet',
+      rpcUrl
+    },
+    {
+      id: 42161,
+      token: 'ARB-ETH',
+      label: 'Arbitrum One',
+      rpcUrl: 'https://rpc.ankr.com/arbitrum'
+    },
+    {
+      id: '0xa4ba',
+      token: 'ARB',
+      label: 'Arbitrum Nova',
+      rpcUrl: 'https://nova.arbitrum.io/rpc'
+    },
+    {
+      id: '0x2105',
+      token: 'ETH',
+      label: 'Base',
+      rpcUrl: 'https://mainnet.base.org'
+    }
+  ]
+})
 
 function App() {
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+  console.log("🚀 ~ App ~ wallet:", wallet)
+
+  // create an ethers provider
+  let ethersProvider
+
+  if (wallet) {
+    // if using ethers v6 this is:
+    // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+    ethersProvider = new ethers.providers.Web3Provider(wallet.provider, 'any')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button disabled={connecting} onClick={() => (wallet ? disconnect(wallet) : connect())}>
+        {connecting ? 'connecting' : wallet ? 'disconnect' : 'connect'}
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
